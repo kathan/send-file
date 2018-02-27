1e9ec2f5ccf5dc3e9d0912a59b06badc2d2b3e5c
Darrel Kathan <Darrel.Kathan@grainger.com>
2018-02-27 13:08:45 -0600

    fixed formatting

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var FormData = require(),
+var FormData = require('form-data'),
     fs = require('fs'),
     sendFile = function(url, file, data, callback){
       if(typeof data === 'function'){
         callback = data;
         data = {};
       }
-  		if(fs.existsSync(file)){
-  			var form = new FormData();
-  			for(var i in data){
-  			  form.append(i, data[i]);
-  			}
-  			form.append('image', fs.createReadStream(file));
-  			
-  			form.submit(url, function(err, reply) {
-  				if(err){return callback(err, false, reply);}
-  				if(reply.statusCode === 200){
-  					return callback(null, true, reply);
-  				}
-  				reply.resume(); // for node-0.10.x 
-  				return callback(null, false, reply);
-  			});
-  		}else{
-  			return callback(file+' does not exist.', false);
-  		}
-  	};
+      if(fs.existsSync(file)){
+        var form = new FormData();
+        for(var i in data){
+          form.append(i, data[i]);
+        }
+        form.append('file', fs.createReadStream(file));
+        
+        form.submit(url, function(err, reply) {
+          if(err){return callback(err, false, reply);}
+          if(reply.statusCode === 200){
+            return callback(null, true, reply);
+          }
+          reply.resume(); // for node-0.10.x 
+          return callback(null, false, reply);
+        });
+      }else{
+        return callback(`${file} does not exist.`, false);
+      }
+    };
