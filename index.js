c8a0d3f8975444d76b938e5a0bd8cf96ad55b0bb
Darrel Kathan <Darrel.Kathan@grainger.com>
2018-03-05 10:20:15 -0600

    multi-file support

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-var FormData = require('form-data'),
-    fs = require('fs'),
-    sendFile = function(url, file, data, callback){
-      if(typeof data === 'function'){
-        callback = data;
-        data = {};
-      }
+const FormData = require('form-data');
+const fs = require('fs');
+const sendFile = function(url, files, data, callback){
+  if(typeof data === 'function'){
+    callback = data;
+    data = {};
+  }
 
-      if(fs.existsSync(file)){
-        var form = new FormData();
-        for(var i in data){
-          form.append(i, data[i]);
-        }
-        form.append('file', fs.createReadStream(file));
-        form.submit(url, function(err, reply) {
-          if(err){return callback(err, false, reply);}
-          if(reply.statusCode === 200){
-            return callback(null, true, reply);
-          }
-          reply.resume(); // for node-0.10.x 
-          return callback(null, false, reply);
-        });
+  var form = new FormData();
+  for(var i in data){
+    form.append(i, data[i]);
+  }
+  if(Array.isArray(files)){
+    for(var i in files){
+      if(fs.existsSync(files[i])){
+        //console.log(`Appending files[i]`);
+        form.append('file'+i, fs.createReadStream(files[i]));
       }else{
-        return callback(`${file} does not exist.`, false);
+        return callback(`${files[i]} does not exist.`, false);
       }
-    };
+    }
+  }else{
+    if(fs.existsSync(files)){
+      form.append('file', fs.createReadStream(files));
+    }else{
+      return callback(`${files} does not exist.`, false);
+    }
+  }
+  form.submit(url, function(err, reply) {
+    if(err){return callback(err, false, reply);}
+    if(reply.statusCode === 200){
+      return callback(null, true, reply);
+    }
+    //reply.end(); // for node-0.10.x 
+    return callback(null, false, reply);
+  });
+};
     
 module.exports = sendFile;
--- test/test.js
+++ test/test.js
@@ -3,21 +3,42 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const formidable = require('formidable');
 const app = express();
 const port = 4536;
 
 //==== Start Server ====
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(fileUpload());
+app.use((req, res, next)=>{
+  if (req.method.toLowerCase() == 'post') {
+    var form = new formidable.IncomingForm();
+    //console.log('Server received post. Parsing files...');
+    form.parse(req, function(err, fields, files) {
+      if(err){return;}
+      req.body = fields;
+      //req.files = files;
+      var files_obj = {};
+      for(var i in Object.keys(files)){
+        files_obj[i] = fs.readFileSync(files[i].path);
+        //files_obj[i] = fs.createReadStream(files[i].path);
+      }
+      req.files = files_obj;
+      console.log(files_obj);
+      next();
+    });
+    return;
+  }
+  next();
+});
+
 app.post('*', (req, res) => {
-  console.log({file_name: req.files.file.name, data: req.files.file.data.toString()});
+  console.log({file_name: req.files.file.name});
   res.send('Success!');
 });
 
 var server = app.listen(port, () => {
-  console.log(`Test app listening on port ${port}!`);
+  //console.log(`Test app listening on port ${port}!`);
   //==== Send File ====
-  sendFile(`http://localhost:${port}`, path.resolve(__dirname, 'test.file'), (err, result, reply)=>{
+  sendFile(`http://localhost:${port}`, path.resolve(__dirname, 'big.file'), (err, result, reply)=>{
     if(reply.statusCode === 200){
       console.log('Success!');
     }else{
